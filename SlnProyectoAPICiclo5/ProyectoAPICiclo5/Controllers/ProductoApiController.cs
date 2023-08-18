a65cfb5c20670b2c90bfd7bed30136e2ff17cd55
Isaac <109640070+isaac7690@users.noreply.github.com>
2023-08-18 02:35:05 -0500

    CRUD Producto terminado

--- SlnProyectoAPICiclo5/ProyectoAPICiclo5/Controllers/ProductoApiController.cs
+++ SlnProyectoAPICiclo5/ProyectoAPICiclo5/Controllers/ProductoApiController.cs
@@ -9,11 +9,44 @@ namespace ProyectoAPICiclo5.Controllers
     [ApiController]
     public class ProductoApiController : ControllerBase
     {
+        //lista de productos
         [HttpGet("GetProductos")]
         public async Task<ActionResult<List<Producto>>> GetProductos()
         {
             var lista = await Task.Run(() => (new ProductoDAO()).GetProductos());
             return Ok(lista);
         }
+
+        //agregar producto
+        [HttpPost("AgregarProducto")]
+        public async Task<ActionResult<String>> InsertarProducto(Producto producto)
+        {
+            var mensaje = await Task.Run(() => (new ProductoDAO()).AgregarProducto(producto));
+            return Ok(mensaje);
+        }
+
+        //actualizar producto
+        [HttpPut("ActualizarProducto")]
+        public async Task<ActionResult<String>> ActualizarProducto(Producto producto)
+        {
+            var mensaje = await Task.Run(() => (new ProductoDAO()).ActualizarProducto(producto));
+            return Ok(mensaje);
+        }
+
+        //buscar producto por ID
+        [HttpGet("GetProductoxId/{id}")]
+        public async Task<ActionResult<Producto>> GetProductoId(int id)
+        {
+            var producto = await Task.Run(() => (new ProductoDAO()).GetProductoxId(id));
+            return Ok(producto);
+        }
+
+        [HttpDelete("EliminarProducto")]
+        public async Task<ActionResult<String>> EliminarProducto(int id)
+        {
+            var mensaje = await Task.Run(() => (new ProductoDAO()).EliminarProducto(id));
+            return Ok(mensaje);
+        }
+
     }
 }
--- SlnProyectoAPICiclo5/ProyectoAPICiclo5/DAO/ProductoDAO.cs
+++ SlnProyectoAPICiclo5/ProyectoAPICiclo5/DAO/ProductoDAO.cs
@@ -12,6 +12,7 @@ namespace ProyectoAPICiclo5.DAO
             cadena = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("connection");
         }
 
+        //obtener lista de productos
         public IEnumerable<Producto> GetProductos()
         {
             List<Producto> productosList = new List<Producto>();
@@ -37,5 +38,106 @@ namespace ProyectoAPICiclo5.DAO
             }
             return productosList;
         }
+
+        //agregar nuevo producto
+        public string AgregarProducto(Producto producto)
+        {
+
+            string mensaje = "";
+            using(SqlConnection connection = new SqlConnection(cadena))
+            {
+                try
+                {
+                    SqlCommand command = new SqlCommand("sp_insert_producto", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@prmNomProducto", producto.nomProducto);
+                    command.Parameters.AddWithValue("@prmIdMarca", producto.idMarca);
+                    command.Parameters.AddWithValue("@prmModeloProducto", producto.modeloProducto);
+                    command.Parameters.AddWithValue("@prmCantProducto", producto.cantProducto);
+                    command.Parameters.AddWithValue("@prmIdCategoria", producto.idCategoria);
+                }
+                catch(Exception ex) 
+                {
+                    mensaje = ex.Message;
+                }
+            }
+            return mensaje;
+        }
+
+
+        //actualizar producto
+        public string ActualizarProducto(Producto producto)
+        {
+            string mensaje = "";
+            using (SqlConnection connection = new SqlConnection(cadena))
+            {
+                try
+                {
+                    SqlCommand command = new SqlCommand("sp_UpdateProducto", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@prmIdProducto", producto.idProducto);
+                    command.Parameters.AddWithValue("@prmNomProducto", producto.nomProducto);
+                    command.Parameters.AddWithValue("@prmIdMarca", producto.idMarca);
+                    command.Parameters.AddWithValue("@prmModeloProducto", producto.modeloProducto);
+                    command.Parameters.AddWithValue("@prmCantProducto", producto.cantProducto);
+                    command.Parameters.AddWithValue("@prmIdCategoria", producto.idCategoria);
+                }
+                catch (Exception ex)
+                {
+                    mensaje = ex.Message;
+                }
+            }
+            return mensaje;
+        }
+
+        //buscar producto por ID
+        public Producto GetProductoxId(int id)
+        {
+            Producto producto = new Producto();
+            using (SqlConnection connection = new SqlConnection(cadena))
+            {
+                SqlCommand command = new SqlCommand("sp_buscar_productoId", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@prmIdProducto", id);
+                connection.Open();
+                SqlDataReader dr = command.ExecuteReader();
+                while (dr.Read())
+                {
+                    producto.idProducto = dr.GetInt32(0);
+                    producto.nomProducto = dr.GetString(1);
+                    producto.idMarca = dr.GetInt32(2);
+                    producto.modeloProducto = dr.GetString(3);
+                    producto.cantProducto = dr.GetInt32(4);
+                    producto.idCategoria = dr.GetInt32(5);
+                }
+            }
+            return producto;
+        }
+
+        //Eliminar Producto
+        public string EliminarProducto(int id)
+        {
+            string mensaje = "";
+
+            using (SqlConnection connection = new SqlConnection(cadena))
+            {
+                try
+                {
+                    SqlCommand command = new SqlCommand("", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@prmIdCliente", id);
+                    connection.Open();
+                    int c = command.ExecuteNonQuery();
+                    mensaje = $"Eliminado {c} producto";
+                }
+                catch(Exception ex)
+                {
+                    mensaje = ex.Message;
+                }
+            }
+            return mensaje;
+        }
+
+
     }
 }
